45caa7ffc2e3781da9c978b954ff808590789028
Shiraishi-Arata <119672885+Shiraishi-Arata@users.noreply.github.com>
2025-12-29 20:52:45 +0900

    Clean up whitespace in NeoForgeUtils.kt
    Removed unnecessary blank lines in NeoForgeUtils.kt.

--- YukariLauncher/src/main/java/com/arata/yukarilauncher/feature/mod/modloader/NeoForgeUtils.kt
+++ YukariLauncher/src/main/java/com/arata/yukarilauncher/feature/mod/modloader/NeoForgeUtils.kt
@@ -7,7 +7,6 @@ import org.xml.sax.SAXException
 import java.io.IOException
 import java.io.StringReader
 import javax.xml.parsers.SAXParserFactory
-
 class NeoForgeUtils {
     companion object {
         private const val NEOFORGE_METADATA_URL =
@@ -18,12 +17,10 @@ class NeoForgeUtils {
             "https://maven.neoforged.net/releases/net/neoforged/forge/maven-metadata.xml"
         private const val NEOFORGED_FORGE_INSTALLER_URL =
             "https://maven.neoforged.net/releases/net/neoforged/forge/%1\$s/forge-%1\$s-installer.jar"
-
         @Throws(Exception::class)
         private fun downloadVersions(metaDataUrl: String, name: String, force: Boolean): List<String> {
             val parserFactory = SAXParserFactory.newInstance()
             val saxParser = parserFactory.newSAXParser()
-
             return DownloadUtils.downloadStringCached<List<String>>(
                 metaDataUrl,
                 name,
@@ -42,24 +39,20 @@ class NeoForgeUtils {
                 }
             }
         }
-
         @JvmStatic
         @Throws(Exception::class)
         fun downloadNeoForgeVersions(force: Boolean): List<String> {
             return downloadVersions(NEOFORGE_METADATA_URL, "neoforge_versions", force)
         }
-
         @JvmStatic
         @Throws(Exception::class)
         fun downloadNeoForgedForgeVersions(force: Boolean): List<String> {
             return downloadVersions(NEOFORGED_FORGE_METADATA_URL, "neoforged_forge_versions", force)
         }
-
         @JvmStatic
         fun getNeoForgeInstallerUrl(version: String?): String {
             return String.format(NEOFORGE_INSTALLER_URL, version)
         }
-
         @JvmStatic
         fun getNeoForgedForgeInstallerUrl(version: String?): String {
             return String.format(NEOFORGED_FORGE_INSTALLER_URL, version)
