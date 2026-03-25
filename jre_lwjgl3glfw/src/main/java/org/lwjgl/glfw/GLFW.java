4f411f0357f1b8f6d804d008029a4de9aaab20cb
Shiraishi-Arata <adamkhecild15@gmail.com>
2026-03-25 11:10:07 +0000

    lwjgl

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
@@ -20,6 +20,9 @@ import static org.lwjgl.system.Checks.*;
 import static org.lwjgl.system.JNI.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
+
+import com.movtery.zalithlauncher.CursorRegistry;
+
 import java.util.*;
 
 public class GLFW
@@ -314,14 +317,16 @@ public class GLFW
     GLFW_STICKY_KEYS          = 0x33002,
     GLFW_STICKY_MOUSE_BUTTONS = 0x33003,
     GLFW_LOCK_KEY_MODS        = 0x33004,
-    GLFW_RAW_MOUSE_MOTION     = 0x33005;
+    GLFW_RAW_MOUSE_MOTION     = 0x33005,
+    GLFW_UNLIMITED_MOUSE_BUTTONS = 0x33006,
+    GLFW_IME = 0x33007;
 
     /** Cursor state. */
     public static final int
     GLFW_CURSOR_NORMAL   = 0x34001,
     GLFW_CURSOR_HIDDEN   = 0x34002,
-    GLFW_CURSOR_DISABLED = 0x34003;
-
+    GLFW_CURSOR_DISABLED = 0x34003,
+    GLFW_CURSOR_CAPTURED = 0x34004;
     /** The regular arrow cursor shape. */
     public static final int GLFW_ARROW_CURSOR = 0x36001;
 
@@ -492,6 +497,9 @@ public class GLFW
     /* volatile */ public static GLFWWindowPosCallback mGLFWWindowPosCallback;
     /* volatile */ public static GLFWWindowRefreshCallback mGLFWWindowRefreshCallback;
     /* volatile */ public static GLFWWindowSizeCallback mGLFWWindowSizeCallback;
+    public static GLFWPreeditCallback mGLFWPreeditCallback;
+    public static GLFWIMEStatusCallback mGLFWIMEStatusCallback;
+    public static GLFWPreeditCandidateCallback mGLFWPreeditCandidateCallback;
 
     volatile public static int mGLFWWindowWidth, mGLFWWindowHeight;
 
@@ -607,7 +615,7 @@ public class GLFW
         throw new UnsupportedOperationException();
     }
 
-    private static final SharedLibrary GLFW = Library.loadNative(GLFW.class, "org.lwjgl.glfw", "libpojavexec.so", true);
+    public static final SharedLibrary GLFW = Library.loadNative(GLFW.class, "org.lwjgl.glfw", "libpojavexec.so", true);
 
     /** Contains the function pointers loaded from the glfw {@link SharedLibrary}. */
     public static final class Functions {
@@ -802,6 +810,28 @@ public class GLFW
         return lastCallback;
     }
 
+    public static GLFWPreeditCallback glfwSetPreeditCallback(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("GLFWpreeditfun") GLFWPreeditCallbackI cbfun) {
+        GLFWPreeditCallback lastCallback = mGLFWPreeditCallback;
+        if (cbfun == null) mGLFWPreeditCallback = null;
+        else mGLFWPreeditCallback = GLFWPreeditCallback.create(cbfun);
+
+        return lastCallback;
+    }
+    public static GLFWIMEStatusCallback glfwSetIMEStatusCallback(@NativeType("GLFWwindow *") long window, @NativeType("GLFWimestatusfun") @Nullable GLFWIMEStatusCallbackI cbfun) {
+        GLFWIMEStatusCallback lastCallback = mGLFWIMEStatusCallback;
+        if (cbfun == null) mGLFWIMEStatusCallback = null;
+        else mGLFWIMEStatusCallback = GLFWIMEStatusCallback.create(cbfun);
+
+        return lastCallback;
+    }
+    public static GLFWPreeditCandidateCallback glfwSetPreeditCandidateCallback(@NativeType("GLFWwindow *") long window, @NativeType("GLFWpreeditcandidatefun") @Nullable GLFWPreeditCandidateCallbackI cbfun) {
+        GLFWPreeditCandidateCallback lastCallback = mGLFWPreeditCandidateCallback;
+        if (cbfun == null) mGLFWPreeditCandidateCallback = null;
+        else mGLFWPreeditCandidateCallback = GLFWPreeditCandidateCallback.create(cbfun);
+
+        return lastCallback;
+    }
+
     public static GLFWWindowSizeCallback glfwSetWindowSizeCallback(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("GLFWwindowsizefun") GLFWWindowSizeCallbackI cbfun) {
         GLFWWindowSizeCallback lastCallback = mGLFWWindowSizeCallback;
         if (cbfun == null) mGLFWWindowSizeCallback = null;
@@ -833,6 +863,10 @@ public class GLFW
 
     public static int glfwGetPlatform() {
         return GLFW_PLATFORM_X11;
+    }
+
+    public static boolean glfwPlatformSupported(int platform) {
+        return platform == GLFW_PLATFORM_X11;
     }
 
     @NativeType("GLFWwindow *")
@@ -898,8 +932,6 @@ public class GLFW
     }
 
     public static int glfwGetWindowAttrib(@NativeType("GLFWwindow *") long window, int attrib) {
-        if (attrib == GLFW_CONTEXT_VERSION_MAJOR) return 4; // TODO: report actual GL version or add an option for users to select the version
-        if (attrib == GLFW_CONTEXT_VERSION_MINOR) return 6;
         return internalGetWindow(window).windowAttribs.getOrDefault(attrib, 0);
     }
 
@@ -1011,9 +1043,33 @@ public class GLFW
         win.width = mGLFWWindowWidth;
         win.height = mGLFWWindowHeight;
         win.title = title;
+        win.windowAttribs.put(GLFW_RESIZABLE, GLFW_FALSE);
+        // I don't understand why Minecraft doesn't set this itself or why it crashes trying to read
+        // it before set when it controls the cursor status...
+        win.inputModes.put(GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        win.inputModes.put(GLFW_STICKY_KEYS, GLFW_FALSE); // TODO: Fix glfwGetKeyName() to support this
+        win.inputModes.put(GLFW_STICKY_MOUSE_BUTTONS, GLFW_FALSE); // TODO: Fix glfwGetMouseButton() to support this
+        win.inputModes.put(GLFW_IME, GLFW_FALSE);
 
         win.windowAttribs.put(GLFW_HOVERED, 1);
         win.windowAttribs.put(GLFW_VISIBLE, 1);
+
+        // Set the Open GL version for context because Forge and derivatives ask for it
+        // Default on 3.3 because mod compat
+        int glMajor = 3;
+        int glMinor = 3;
+        // Custom defaults for specific renderers
+        if (System.getenv("POJAV_RENDERER").equals("vulkan_zink")) {
+            glMajor = 4;
+            glMinor = 6;
+        } else if (System.getenv("POJAV_RENDERER").equals("gallium_virgl")) {
+            glMajor = 4;
+        } else if (System.getenv("POJAV_RENDERER").equals("opengles3")) {
+            glMajor = 4;
+            glMinor = 0;
+        }
+        win.windowAttribs.put(GLFW_CONTEXT_VERSION_MAJOR, glMajor);
+        win.windowAttribs.put(GLFW_CONTEXT_VERSION_MINOR, glMinor);
 
         mGLFWWindowMap.put(ptr, win);
         mainContext = ptr;
@@ -1038,6 +1094,10 @@ public class GLFW
             e.printStackTrace();
         }
         nglfwSetShowingWindow(mGLFWWindowMap.size() == 0 ? 0 : mGLFWWindowMap.keyAt(mGLFWWindowMap.size() - 1));
+    }
+
+    public static String glfwGetWindowTitle(long window) {
+        return internalGetWindow(window).title.toString();
     }
 
     public static void glfwDefaultWindowHints() {
@@ -1178,9 +1238,6 @@ public class GLFW
     }
 
     public static int glfwGetKey(@NativeType("GLFWwindow *") long window, int key) {
-		// This is jank, anything asking for int 348 results in an IndexOutOfBounds because idk.
-        // Probably an off-by-one error. This is the 'fix'
-        if (key == GLFW_KEY_LAST){return GLFW_KEY_LAST;}
         return keyDownBuffer.get(Math.max(0, key-31));
     }
 
@@ -1207,13 +1264,15 @@ public class GLFW
     }*/
 
     public static long glfwCreateCursor(@NativeType("const GLFWimage *") GLFWImage image, int xhot, int yhot) {
-        return 4L;
+        return CursorRegistry.getDefaultCursor();
     }
     public static long glfwCreateStandardCursor(int shape) {
-        return 4L;
+        return CursorRegistry.registerCursor(shape);
     }
     public static void glfwDestroyCursor(@NativeType("GLFWcursor *") long cursor) {}
-    public static void glfwSetCursor(@NativeType("GLFWwindow *") long window, @NativeType("GLFWcursor *") long cursor) {}
+    public static void glfwSetCursor(@NativeType("GLFWwindow *") long window, @NativeType("GLFWcursor *") long cursor) {
+        CallbackBridge.nativeSetCursorShape(CursorRegistry.getShape(cursor));
+    }
 
     public static boolean glfwRawMouseMotionSupported() {
         // Should be not supported?
@@ -1473,6 +1532,6 @@ public class GLFW
     public static void glfwMaximizeWindow(@NativeType("GLFWwindow *") long window) {
     }
 
-	public static void glfwRestoreWindow(@NativeType("GLFWwindow *") long window) {
-	}
+    public static void glfwRestoreWindow(@NativeType("GLFWwindow *") long window) {
+    }
 }
