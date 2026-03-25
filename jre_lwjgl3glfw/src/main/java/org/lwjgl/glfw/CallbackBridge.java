df7a2d400b82ce1fdbf04858af30a5db8a4d04ed
Shiraishi-Arata <adamkhecild15@gmail.com>
2026-03-25 12:34:01 +0000

    re

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/CallbackBridge.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/CallbackBridge.java
@@ -46,6 +46,5 @@ public class CallbackBridge {
     public static native boolean nativeSetInputReady(boolean ready);
     public static native String nativeClipboard(int action, byte[] copy);
     public static native void nativeSetGrabbing(boolean grab);
-    public static native void nativeSetCursorShape(int shape);
 }
 
